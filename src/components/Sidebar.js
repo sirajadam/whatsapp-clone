4d9e4d1ca3841a812bd94195706cdc9a86754f84
adam <adamsirajdeveloper@gmail.com>
2021-09-12 00:30:57 +0200

    added sidebar header and sidebar search

---
+++ src/components/Sidebar.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import "./Sidebar.css";
+import SidebarChat from "./SidebarChat";
+import { SearchOutlined } from "@material-ui/icons";
+
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__header__right">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <div className="sidebar__search__container">
+          <SearchOutlined />
+          <input type="text" placeholder="Search or start new chat" />
+        </div>
+      </div>
+      <div className="sidebar__chats">
+        <SidebarChat />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCQbUzzWpQbPBOcpRnNU2BpJl41qJRisFY",
+  authDomain: "whatsapp-clone-b49f5.firebaseapp.com",
+  projectId: "whatsapp-clone-b49f5",
+  storageBucket: "whatsapp-clone-b49f5.appspot.com",
+  messagingSenderId: "572513242193",
+  appId: "1:572513242193:web:82685414e4da629523bc70",
+};
